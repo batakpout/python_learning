12f5633e5c43d9304b8d785ea3ea71ed3452e926
aamir <16462830+batakpout@users.noreply.github.com>
2025-04-23 07:22:39 +0530

    Add files via upload

--- ds_algo/array1d/subset_of_array.py
+++ ds_algo/array1d/subset_of_array.py
@@ -1,12 +1,13 @@
 import math
 
+
 def subSets(input_arr):
     n = len(input_arr)
     total_subsets = int(math.pow(2, n))
     for i in range(0, total_subsets):
         result = ""
         dec = i
-        for j in range(n-1, -1, -1): # goes from 2 to 0 if n is 3
+        for j in range(n - 1, -1, -1):  # goes from 2 to 0 if n is 3
             rem = dec % 2
             if rem == 0:
                 result = "_" + result
@@ -18,6 +19,17 @@ def subSets(input_arr):
     print()
 
 
+"""
+time complexity:
+Outer loop: O(2^n)
+Inner loop: O(n) per outer iteration
+Total: O(2^n * n)
+
+Space Complexity:
+The space complexity is O(n) for the result string (since it stores at most n characters at a time).
+ The rest of the variables use constant space.
+"""
 if __name__ == "__main__":
-    char_arr = "123" # or ['a', 'b', 'c']
+    char_arr = "abc"  # or ['a', 'b', 'c']
     subSets(char_arr)
+# sub sequences
